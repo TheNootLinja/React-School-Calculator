67f278bdbfcba932628279ee606d6d1053ff5713
TheNootLinja <thenootlinja@gmail.com>
2022-01-13 13:18:29 -0500

    Initial commit of project along with pieces of layout for the calculator

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="calc-bg">
+        <div className="calc-outer-screen">
+          <div className="calc-inner-screen"></div>
+        </div>
+        <button className="calc-rnd-btn">1</button>
+        <button className="calc-rnd-btn">2</button>
+        <button className="calc-rnd-btn">3</button>
+        <button className="calc-rnd-btn">4</button>
+        <button className="calc-rnd-btn">5</button>
+        <button className="calc-rnd-btn">6</button>
+        <button className="calc-rnd-btn">7</button>
+        <button className="calc-rnd-btn">8</button>
+        <button className="calc-rnd-btn">9</button>
+      </div>
     </div>
   );
 }
