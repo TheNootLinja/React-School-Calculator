c449dc0653664cff0fae26d8b84b6d5b9b5b2e08
TheNootLinja <thenootlinja@gmail.com>
2022-01-13 22:10:58 -0500

    Add further styling along with showing input on calculator screen.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,58 @@
 import './App.css';
+import {useState} from 'react';
 
 function App() {
+
+  const [calcString, setCalcString] = useState('');
+
+  // let calcString = '';
+
+  function handleClick(e) {
+    setCalcString(calcString + e.target.innerText);
+  }
+
+  function handleClearClick() {
+    setCalcString('');
+  }
+
   return (
-    <div className="App">
-      <div className="calc-bg">
-        <div className="calc-outer-screen">
-          <div className="calc-inner-screen"></div>
+    <div className='App'>
+      <div className='calc-bg'>
+        <div className='calc-outer-screen'>
+          <div className='calc-inner-screen'><p>{ calcString }</p></div>
         </div>
-        <button className="calc-rnd-btn">1</button>
-        <button className="calc-rnd-btn">2</button>
-        <button className="calc-rnd-btn">3</button>
-        <button className="calc-rnd-btn">4</button>
-        <button className="calc-rnd-btn">5</button>
-        <button className="calc-rnd-btn">6</button>
-        <button className="calc-rnd-btn">7</button>
-        <button className="calc-rnd-btn">8</button>
-        <button className="calc-rnd-btn">9</button>
+        <div className='sp-section'>
+          <div className='sp-inner'></div>
+        </div>
+        <div className="outer-btn-cont">
+          <div className='btn-cont-1'>
+            <button className='calc-red-btn calc-btn-reg' onClick={handleClick}>+/-</button>
+            <button className='calc-red-btn calc-btn-reg' onClick={handleClick}>/</button>
+            <button className='calc-red-btn calc-btn-reg' onClick={handleClick}>%</button>
+            <button className='calc-red-btn calc-btn-reg' onClick={handleClick}>MRC</button>
+            <button className='calc-red-btn calc-btn-reg' onClick={handleClick}>M-</button>
+            <button className='calc-red-btn calc-btn-reg' onClick={handleClick}>M+</button>
+            <button className='calc-wht-btn calc-btn-reg' onClick={handleClick}>7</button>
+            <button className='calc-wht-btn calc-btn-reg' onClick={handleClick}>8</button>
+            <button className='calc-wht-btn calc-btn-reg' onClick={handleClick}>9</button>
+            <button className='calc-wht-btn calc-btn-reg' onClick={handleClick}>4</button>
+            <button className='calc-wht-btn calc-btn-reg' onClick={handleClick}>5</button>
+            <button className='calc-wht-btn calc-btn-reg' onClick={handleClick}>6</button>
+            <button className='calc-wht-btn calc-btn-reg' onClick={handleClick}>1</button>
+            <button className='calc-wht-btn calc-btn-reg' onClick={handleClick}>2</button>
+            <button className='calc-wht-btn calc-btn-reg' onClick={handleClick}>3</button>
+            <button className='calc-red-btn calc-btn-reg' onClick={handleClearClick}>ON/C</button>
+            <button className='calc-wht-btn calc-btn-reg' onClick={handleClick}>0</button>
+            <button className='calc-wht-btn calc-btn-reg' onClick={handleClick}>.</button>
+          </div>
+          <div className='btn-cont-2'>
+          <button className='calc-red-btn calc-btn-reg' onClick={handleClick}>%</button>
+            <button className='calc-red-btn calc-btn-reg' onClick={handleClick}>x</button>
+            <button className='calc-red-btn calc-btn-reg' onClick={handleClick}>-</button>
+            <button className='calc-red-btn calc-btn-reg' onClick={handleClick}>+</button>
+            <button className='calc-red-btn calc-btn-reg calc-btn-lg' onClick={handleClick}>=</button>
+          </div>
+        </div>
       </div>
     </div>
   );
